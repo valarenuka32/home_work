6361fb819ffafb7000bede8e5537505dd1ff34e0
valarenuka32 <valarenuka32@gmail.com>
2023-09-04 23:25:41 +0530

    add file

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,4 @@ module.exports.user = require("./user.model");
 module.exports.category = require("./category.model");
 module.exports.product = require("./product.model");
 module.exports.grocery = require("./grocery.model");
+module.exports.fashion = require("./fashion.model");
