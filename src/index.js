de98adc05a568707d75abdc640dc0a073d30432c
valarenuka32 <valarenuka32@gmail.com>
2023-09-04 09:12:21 +0530

    add file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(config.port, () => {
+    console.log(`server is started port no ${config.port}`);
+});
+
