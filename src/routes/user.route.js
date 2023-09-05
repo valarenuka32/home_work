cdd4d9a889c39dbd6bdd4a9e7f09a7a1ee58a3f6
valarenuka32 <valarenuka32@gmail.com>
2023-09-05 22:17:52 +0530

    add file

---
+++ src/routes/user.route.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const { userValidation } = require("../validations");
+const { userCantroller } = require("../controllers");
+const validate=require("../middlewares/")
+
