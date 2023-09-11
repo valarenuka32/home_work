9001f453c1f316da9c64bbbb1d0f0e7ddac0252d
valarenuka32 <valarenuka32@gmail.com>
2023-09-11 14:02:44 +0530

    add file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const http = require("http");
 const fs = require("fs");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const config = require("./config/config");
-const routes=require("./routes")
+const routes = require("./routes");
 const { connectDB } = require("./db/dbconnection");
 
 // Database connection
@@ -14,6 +15,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
+
+/** enable cors */
+app.use(cors());
+app.options("*", cors());
 
 // route namespace
 app.use("/v1", routes);
