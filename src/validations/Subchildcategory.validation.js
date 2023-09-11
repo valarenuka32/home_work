6202cffb96d77c680add1c0b1b3ba94e86899aed
valarenuka32 <valarenuka32@gmail.com>
2023-09-11 09:48:15 +0530

    add file

--- src/validations/Subchildcategory.validation.js
+++ src/validations/Subchildcategory.validation.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const createSubchildCategory = {
     body: Joi.object().keys({
         SubchildCategory_name: Joi.string().required().trim(),
-        SubchildCategory_desc: Joi.number().required().trim(),
+        SubchildCategory_desc: Joi.string().required().trim(),
     }),
 };
 
