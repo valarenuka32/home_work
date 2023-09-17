c51f875663406789a54ecb88647f034bd97c5336
valarenuka32 <valarenuka32@gmail.com>
2023-09-17 15:00:09 +0530

    add file

--- src/config/config.js
+++ src/config/config.js
@@ -6,11 +6,18 @@ dotenv.config();
 const envVarsSchema = joi.object({
     PORT: joi.number().default(7000),
     MONGODB_URL: joi.string().trim().description("mongoodb url"),
+    JWT_SECRET_KEY: joi.string()
+        .description("Jwt sectreat key")
+        .default("thisisJwtsecreat_key"),
 }).unknown();
 
 const { value: envVars, error } = envVarsSchema
     .prefs({ errors: { label: "key" } })
     .validate(process.env);
+
+if (error) {
+    console.log("Config Error: ", error);
+}
 
 module.exports = {
     port: envVars.PORT,
@@ -20,5 +27,8 @@ module.exports = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         },
+    },
+    jwt:{
+        secret_key:envVars.JWT_SECRET_KEY,
     },
 };
