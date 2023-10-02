6a1e1d4d2657874b9547082205b9e8ec1e8cdf2d
valarenuka32 <132243973+valarenuka32@users.noreply.github.com>
2023-10-02 09:32:14 +0530

    add file

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -17,18 +17,18 @@ const sendMail = async (to, subject, text) => {
       from: config.email.from,
       to,
       subject,
-      text,
-      html: `
-      <div
-        class="container"
-        style="max-width: 90%; margin: auto; padding-top: 20px"
-      >
-        <h2>Welcome to the club.</h2>
-        <h4>You are officially In ✔</h4>
-        <p style="margin-bottom: 30px;">Pleas enter the sign up OTP to get started</p>
-        <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;">${ ("0".repeat(4) + Math.floor(Math.random() * 10 ** 4)).slice(-4)}</h1>
-   </div>
-      `
+      text
+      // html: `
+  //     <div
+  //       class="container"
+  //       style="max-width: 90%; margin: auto; padding-top: 20px"
+  //     >
+  //       <h2>Welcome to the club.</h2>
+  //       <h4>You are officially In ✔</h4>
+  //       <p style="margin-bottom: 30px;">Pleas enter the sign up OTP to get started</p>
+  //       <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;">${ ("0".repeat(4) + Math.floor(Math.random() * 10 ** 4)).slice(-4)}</h1>
+  //  </div>
+  //     `
     });
   } catch (error) {
     return false;
